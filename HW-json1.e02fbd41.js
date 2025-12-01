aec51dca5a55725ae8ed14c90f6cb160cf85bf73
NastifyMe <suprunar2010@gmail.com>
2025-12-01 21:20:01 +0100

    Updates

--- HW-json1.e02fbd41.js
+++ HW-json1.e02fbd41.js
@@ -714,45 +714,42 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"jOXmm":[function(require,module,exports,__globalThis) {
-// const addBookmarkBtn = document.getElementById('addBookmarkBtn')
-// const bookmarkInput = document.getElementById('bookmarkInput')
-// const bookmarkList = document.getElementById('bookmarkList')
-// const saved = localStorage.getItem('links')
-// let links
-// if (saved){
-//     links = JSON.parse(saved)
-// } else {
-//     links = []
-// }
-// // localStorage.clear()
-// render()
-// function render() {
-//     bookmarkList.innerHTML = ''
-//     links.forEach((link, index) => {
-//         const li = document.createElement('li')
-//         const a = document.createElement('a')
-//         a.textContent = link
-//         a.classList.add('a')
-//         const deleteBtn = document.createElement('button')
-//         deleteBtn.textContent = 'delete'
-//         deleteBtn.classList.add('delete')
-//         deleteBtn.addEventListener('click', () => deleteLink(index))
-//         li.appendChild(a)
-//         li.appendChild(deleteBtn)
-//         bookmarkList.appendChild(li)
-//     });
-//     localStorage.setItem('links', JSON.stringify(links))
-// }
-// function deleteLink(index) {
-//     links.splice(index, 1)
-//     render()
-// }
-// addBookmarkBtn.addEventListener('click', () => {
-//     const value = bookmarkInput.value
-//     links.push(value)
-//     render()
-//     bookmarkInput.value = " "
-// })
+const addBookmarkBtn = document.getElementById('addBookmarkBtn');
+const bookmarkInput = document.getElementById('bookmarkInput');
+const bookmarkList = document.getElementById('bookmarkList');
+const saved = localStorage.getItem('links');
+let links;
+if (saved) links = JSON.parse(saved);
+else links = [];
+// localStorage.clear()
+render();
+function render() {
+    bookmarkList.innerHTML = '';
+    links.forEach((link, index)=>{
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.textContent = link;
+        a.classList.add('a');
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = 'delete';
+        deleteBtn.classList.add('delete');
+        deleteBtn.addEventListener('click', ()=>deleteLink(index));
+        li.appendChild(a);
+        li.appendChild(deleteBtn);
+        bookmarkList.appendChild(li);
+    });
+    localStorage.setItem('links', JSON.stringify(links));
+}
+function deleteLink(index) {
+    links.splice(index, 1);
+    render();
+}
+addBookmarkBtn.addEventListener('click', ()=>{
+    const value = bookmarkInput.value;
+    links.push(value);
+    render();
+    bookmarkInput.value = " ";
+});
 const username = document.getElementById('username');
 const password = document.getElementById('password');
 const saveBtn = document.getElementById('saveBtn');
