5e74484b42d902e199356f610c54487b3587dc31
NastifyMe <suprunar2010@gmail.com>
2025-12-01 21:47:13 +0100

    hw-json

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
-// const addBookmarkBtn = document.getElementById('addBookmarkBtn')
-// const bookmarkInput = document.getElementById('bookmarkInput')
-// const bookmarkList = document.getElementById('bookmarkList')
+const addBookmarkBtn = document.getElementById('addBookmarkBtn')
+const bookmarkInput = document.getElementById('bookmarkInput')
+const bookmarkList = document.getElementById('bookmarkList')
 
-// const saved = localStorage.getItem('links')
-// let links
-// if (saved){
-//     links = JSON.parse(saved)
-// } else {
-//     links = []
-// }
-// // localStorage.clear()
-// render()
+const saved = localStorage.getItem('links')
+let links
+if (saved){
+    links = JSON.parse(saved)
+} else {
+    links = []
+}
+// localStorage.clear()
+render()
 
-// function render() {
-//     bookmarkList.innerHTML = ''
-//     links.forEach((link, index) => {
-//         const li = document.createElement('li')
+function render() {
+    bookmarkList.innerHTML = ''
+    links.forEach((link, index) => {
+        const li = document.createElement('li')
 
-//         const a = document.createElement('a')
-//         a.textContent = link
-//         a.classList.add('a')
+        const a = document.createElement('a')
+        a.textContent = link
+        a.classList.add('a')
 
-//         const deleteBtn = document.createElement('button')
+        const deleteBtn = document.createElement('button')
 
-//         deleteBtn.textContent = 'delete'
-//         deleteBtn.classList.add('delete')
+        deleteBtn.textContent = 'delete'
+        deleteBtn.classList.add('delete')
 
 
-//         deleteBtn.addEventListener('click', () => deleteLink(index))
+        deleteBtn.addEventListener('click', () => deleteLink(index))
 
-//         li.appendChild(a)
-//         li.appendChild(deleteBtn)
-//         bookmarkList.appendChild(li)
-//     });
-//     localStorage.setItem('links', JSON.stringify(links))
-// }
+        li.appendChild(a)
+        li.appendChild(deleteBtn)
+        bookmarkList.appendChild(li)
+    });
+    localStorage.setItem('links', JSON.stringify(links))
+}
 
-// function deleteLink(index) {
-//     links.splice(index, 1)
-//     render()
-// }
+function deleteLink(index) {
+    links.splice(index, 1)
+    render()
+}
 
-// addBookmarkBtn.addEventListener('click', () => {
-//     const value = bookmarkInput.value
-//     links.push(value)
-//     render()
-//     bookmarkInput.value = " "
-// })
+addBookmarkBtn.addEventListener('click', () => {
+    const value = bookmarkInput.value
+    links.push(value)
+    render()
+    bookmarkInput.value = " "
+})
 
 
 
@@ -66,7 +66,7 @@ const username = document.getElementById('username')
 const password = document.getElementById('password')
 const saveBtn = document.getElementById('saveBtn')
 
-function render() {
+function render2() {
     username.value = JSON.parse(localStorage.getItem('user'))
     password.value = JSON.parse(localStorage.getItem('pass'))
 }
@@ -81,4 +81,4 @@ saveBtn.addEventListener('click', () => {
     password.value = ''
 })
 
-render()
+render2()
